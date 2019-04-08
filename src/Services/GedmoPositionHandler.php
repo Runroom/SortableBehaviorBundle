aa241a3d1e287ee3101eb54863b0a3aba9126a80
Jordi <jordism91@gmail.com>
2019-04-08 10:54:50 +0200

    Fix typo in namespace

--- src/Services/GedmoPositionHandler.php
+++ src/Services/GedmoPositionHandler.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace Runroom\SortableBehaviorBundle\Service;
+namespace Runroom\SortableBehaviorBundle\Services;
 
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Mapping\ClassMetadata;
